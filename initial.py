92865694123e4b234feb4dc81fb727d6c5eaac50
Hansenyassine4585 <79019794+Hansenyassine4585@users.noreply.github.com>
2022-01-31 15:05:14 -0800

    initial.py
    initial.py

---
+++ initial.py
@@ -0,0 +1,9 @@
+# Yassine Touil
+# Fun Fact: I like numbers, and I enjoy playing soccer
+print("Y   Y   TTTTT")
+print(" Y Y      T  ")
+print("  Y       T  ")
+print("  Y       T  ")
+print("  Y       T  ")
+print("  Y       T  ")
+print("  Y       T  ")
